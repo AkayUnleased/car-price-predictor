9f82de3e02ee00cc39c5efddc6994e114e449714
AkayUnleased <aakritikheterpal398@gmail.com>
2026-02-03 12:27:23 +0530

    bind port for render

--- application.py
+++ application.py
@@ -43,6 +43,11 @@ def predict():
 
 
 
-if __name__=='__main__':
-    app.run()
+import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
 
